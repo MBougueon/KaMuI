0aaba4f34e4c390b48d8e8482f960b2b1a677ac1
mbougueon <matthieu.bougueon@hotmail.fr>
2024-04-09 17:23:12 +0200

    Gibbs sampler and MH/MCMC update

--- model_fitting/MCMC.py
+++ model_fitting/MCMC.py
@@ -3,64 +3,66 @@
 
 import distribution as dis
 import numpy as np
-import pandas as pd
 
 
-
-    
-
-def sampling(theta, other_theta, model_distri):
-    """Sampling of parameter for Gibbs sampler"""
-    # mean_other_theta = np.mean(other_theta)
-    # var_other_theta = np.var(other_theta)
-    # precision_other_theta = 1 / var_other_theta
-
-    #new_theta = np.random.normal(mean_other_theta + precision_other_theta * (y - 2 * x), np.sqrt(1 / (len(y) * precision_other_theta + 1)))
-
-
-def gibbs_sampler(parameters, model_distri):
-    """Gibbs sampler
-    
-    Parameter
-    ---------
-    
-    Parameters: list, list of the parameters estimated
-    
-    Model_distri: XXX, distribution of the model used to infer parameters
-    
+def gibbs_sampler(X, y, theta_other, p, distribution):
     """
-    for p in range (0,len(parameters)):
-        if p == 0:
-            other_p = parameters[:p]
-        if p == len(parameters):
-            other_p = parameters[p:]
-        else:
-            other_p = parameters[p+1:]+parameters[:p]    
-        p = sampling(parameters[p], other_p, model_distri)
-                      
-#estimer chaque paramètre en fonction des autres
-    
-#Définir équation d'échantillonage
+    Samples a parameter theta_i from its conditional distribution given other parameters.
 
-def metropolis_hasting(parameters, prior_distribution, N=10000, burn_in=0.2, **kwargs):
+    Parameters:
+    -----------
+    X : numpy.ndarray
+        Design matrix.
+    y : numpy.ndarray
+        Target values vector.
+    theta_other : numpy.ndarray
+        Vector of other parameters.
+    p : int
+        Index of the parameter to sample.
+
+    Returns:
+    --------
+    numpy.ndarray
+        A new sample of theta_i.
+    """
+    # Select the p-th column of the design matrix
+    X_p = X[:, p:p+1]
+    # Get the other parameters
+    X_other = np.delete(X, p, axis=1)
+    # Delete parameter p from the vector of other parameters
+    theta_other = np.delete(theta_other, p)
+
+    if distribution == "normal_multi":
+        # Compute the conditional mean of theta_p
+        mu_p = np.linalg.solve(X_p.T @ X_p, X_p.T @ (y - X_other @ theta_other))
+        # Compute the conditional covariance of theta_p
+        sigma_p = np.linalg.inv(X_p.T @ X_p)
+
+        # Sample theta_p from the multivariate normal distribution
+        return (np.random.multivariate_normal(mu_p.flatten(), sigma_p))
+
+    # elif distribution == "gamma":
+
+
+def metropolis_hasting(current, prior_distribution, **kwargs):
     """
     Metropolis-Hastings algorithm
-    
+
     Parameters
     ----------
-    parameters : list
-        List of the parameters to be estimated.
-    
+    currnet : float
+        value of the paramters estimated.
+
     prior_distribution : string
         Target PDF (Probability Density Function).
-    
+
     N : int, optional
         Number of samples to generate. Default is 10000.
-    
+
     burn_in : float, optional
         Proportion of proposal samples to ignore to allow the algorithm to converge.
         This fraction of the initial samples will be discarded. Default is 0.2.
-    
+
     **kwargs : dict
         Additional keyword arguments for specifying parameters of the prior distribution.
         Possible keys include:
@@ -70,51 +72,80 @@ def metropolis_hasting(parameters, prior_distribution, N=10000, burn_in=0.2, **k
         - theta : float, scale parameter for gamma distribution
     """
 
-    
-        
-    samples = []  # List to store samples for the current parameter
+
+    # Propose a new sample based on the prior distribution
+    proposal = dis.prior_distribution_MH(prior_distribution, current, kwargs['sigma'])
+
+    # Calculate the acceptance criterion based on the prior distribution type
+    if prior_distribution == "normal":
+        acceptance_crit = dis.acceptance_criterion_norm(proposal, current, kwargs['mu'], kwargs['sigma'])
+    elif prior_distribution == "gamma":
+        acceptance_crit = dis.acceptance_criterion_gamma(proposal, current, kwargs['k'], kwargs['theta'])
+
+    # Generate a random number between 0 and 1
+    u = np.random.uniform(0, 1)
+
+    # Accept the proposal with probability equal to the acceptance criterion
+    if u < acceptance_crit:
+        current = proposal
+
+    return(current)
+
+def mcmc(parameters, data, observations, prior_distribution, approach="metropolis_hasting", N = 1000, burn_in=0.2, **kwargs):
+    """
+    MCMC algorithm to estimate parameters using either Metropolis-Hastings or Gibbs sampling.
+
+    Parameters:
+    -----------
+    N : int
+        Number of samples to generate.
+    parameters : list
+        List of the parameters to be estimated.
+    data : numpy.ndarray
+        Data used in the model.
+    approach : str, optional
+        MCMC approach to use: "metropolis_hasting" or "gibbs". Default is "metropolis_hasting".
+    burn_in : float, optional
+        Proportion of samples to ignore for the burn-in phase. Default is 0.2.
+    **kwargs : dict
+        Additional keyword arguments depending on the chosen approach.
+        Possible keys include:
+        - sigma : float, standard deviation for normal distribution
+        - mu : float, mean for normal distribution
+        - k : float, shape parameter for gamma distribution
+        - theta : float, scale parameter for gamma distribution
+
+    Returns:
+    --------
+    list
+        List of samples for each parameter.
+    """
+    all_samples = []  # List to store all samples for each parameter
     idx_burn_in = int(burn_in * N)  # Index to start considering samples after burn-in period
-    current = p # Initialize the current value of the parameter
+
+
 
     for i in range(N):  # Generate N samples
-        
-        # Propose a new sample based on the prior distribution
-        proposal = dis.prior_distribution_MH(prior_distribution, current, kwargs['sigma'])
-        
-        # Calculate the acceptance criterion based on the prior distribution type
-        if prior_distribution == "normal":
-            acceptance_crit = dis.acceptance_criterion_norm(proposal, current, kwargs['mu'], kwargs['sigma'])
-        elif prior_distribution == "gamma":
-            acceptance_crit = dis.acceptance_criterion_gamma(proposal, current, kwargs['k'], kwargs['theta'])
-        
-        # Generate a random number between 0 and 1
-        u = np.random.uniform(0, 1)
-        
-        # Accept the proposal with probability equal to the acceptance criterion
-        if u < acceptance_crit:
-            current = proposal
-        
-        # Store the sample after burn-in period
-        if i >= idx_burn_in:
-            samples.append(current)
-    
-    # Store the samples for the current parameter
-    all_samples.append(samples)
-    
+        for p in range(parameters):  # Loop over each parameter
+            sample = []  # List to store samples for the current parameter
 
+            if approach == "metropolis_hasting":
+                # Use Metropolis-Hastings algorithm to sample the parameter
+                parameters[p] = metropolis_hasting(p, prior_distribution, **kwargs)
 
-def mcmc(n, parameter, data, approach = "metropolis_hasting", **kwargs):
-    """MCMC algorithm
-    
-    Parameter
-    ---------
-    
-    Parameter: list, list of the parameters estimated"""
+            elif approach == "gibbs":
+                # Use Gibbs sampling algorithm to sample the parameter
+                len_ob = len(observations)  # Number of observations
+                len_p = len(parameters)  # Number of parameters
+                X = np.random.randn(len_ob, len_p)  # Design matrix
+                theta_current = parameters
+                y = X @ data  # Simulate data for Gibbs sampling
+                parameters[p] = gibbs_sampler(X, y, theta_current, p, prior_distribution)  # Gibbs sampling step
 
-    all_samples = []  # List to store all samples for each parameter
-    
-    for p in parameters:  # Loop over each parameter
+            if i >= idx_burn_in:  # After the burn-in period
+                sample.append(parameters)  # Store the current parameter sample
 
-        if approach == "metropolis_hasting":
-            samples = metropolis_hasting(parameters, prior_distribution, N=10000, burn_in=0.2, **kwargs)
+        # Store the samples for the current parameter
+        all_samples.append(sample)
+
     return all_samples
